511745712ac95aeb187ba00c79123772544c0bee
hzl091 <hzl091@126.com>
2018-06-08 18:34:47 +0800

    添加测试用例

---
+++ MyFX.Core.Test/Security_Test/Signature_Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyFX.Core.Test.Security_Test
+{
+    [TestClass]
+    public class Signature_Test
+    {
+        [TestMethod]
+        public void Sign_Test()
+        {
+            string rs = MyFX.Core.Security.Signature.Sign(
+                "10010|3c895bff2b524569aecdcdfgd3343434|20180604192327|1|30", "19567b5ae0134267a38931a0089b3196");
+            Console.WriteLine(rs);
+        }
+
+        [TestMethod]
+        public void Verify_Test()
+        {
+            bool isSuccess = MyFX.Core.Security.Signature.Verify("10010|3c895bff2b524569aecdcdfgd3343434|20180604192327|1|30",
+                "19567b5ae0134267a38931a0089b3196", "C39232599A436ADA9A4813E7F4FB7644");
+           Console.WriteLine(isSuccess);
+         Assert.IsTrue(isSuccess);
+        }
+    }
+}
